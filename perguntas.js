5b25490fb46ab6b78c93a8ce2306ed46a1620120
Enzo Leonardo <140806929+enzoLSB@users.noreply.github.com>
2024-11-07 08:33:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França??',
+    'Paris'
+)
+
+criaCartao(
+    'desenho',
+    'Qual é a princesa que perdeu seu sapatinho de cristal??',
+    'Cinderela'
+)
+
+criaCartao(
+    'jogo',
+    'Qual o jogo mais vendido do mundo"?',
+    'teteris'
+)
+
+criaCartao(
+    'animais',
+    'Qual é o maior animal terrestre??',
+    'Elefante'
+)
+
+criaCartao(
+    'jogador',
+    'Qual é o melhor jogador do mundo??',
+    'menino neyy'
+)
+
+criaCartao(
+    'cantor',
+    'Qual é o meaior raper do mundo ',
+    'emimem'
+)
